3036136b2f821ab31009ca4b181c93f12bdba909
Marcus Danielsson <marcus@karmint.com>
2014-06-10 17:17:00 +0200

    merges

--- wp-content/themes/yoiride/index.php
+++ wp-content/themes/yoiride/index.php
@@ -12,19 +12,87 @@
  */
 get_header(); 
 
-if (!is_front_page()) {
+if (!is_front_page()) { ?>
+				<?
+					if(get_post_format() == 'gallery') {
+						get_template_part( 'content', get_post_format() );
+						return;
+					}
+				?>
 
-                              while ( have_posts() ) : the_post();
+                                <section class="yellow-section">
+                                </section>
 
-                                        /*
-                                         * Include the post format-specific template for the content. If you want to
-                                         * use this in a child theme, then include a file called called content-___.php
-                                         * (where ___ is the post format) and that will be used instead.
-                                         */
-                                        get_template_part( 'content', get_post_format() );
+                                        <?php query_posts($query_string . '&cat=-7,-1'); ?>
+                                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<section class="white-section blog-section">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-9">
+								<div class="row"><!-- TITLE POST ROW -->
+									<div class="col-md-12">
+										<a href="<?echo get_author_posts_url( get_the_author_meta( 'ID' ) );?>" class="blog-th">
+										<img src="<?php bloginfo('template_url'); ?>/img/nils_profil.jpg">
+										</a>
+										<h4><a href="<?php the_permalink() ?>"><?php the_title_attribute(); ?></a></h4>
+										<p>
+											<i class="fa fa-user"></i> av <?php the_author_posts_link(); ?>
+											| <i class="fa fa-calendar"></i><?php echo the_time('m F, Y'); ?>
+										</p>
+									</div>
+								</div>
+								<div class="row"><!-- BLOG POST ROW -->
+									<div class="col-md-12"><!-- Post -->
+										<br>
+										<?php the_content( __("Read more &raquo;","wpbootstrap") ); ?>
+										<br>
+									</div>
+								</div>	
 
-                                endwhile;
+                                                                <div class="row">
+                                                                        <div class="col-md-12">
+										<hr>
+                                                                                <p><i class="fa fa-comments"></i> <a href="#">3 Comments</a></p>
+										<?php $tags = get_the_tags(); 
+										if($tags) {
+											
+											echo '<p><i class="fa fa-tags"></i><!-- Taggar: -->';
+											
+											foreach ( $tags as $tag) {
+												echo '<a href="'.get_tag_link( $tag->term_id ).'"><span class="label label-info">'.$tag->name.'</span></a> ';
+											}
+											echo '</p>';
+										}
+										?>
 
+										<?php $cats = get_the_category(); ?>
+										<?
+										if($cats) {
+											echo '<p><i class="fa fa-folder-open"></i><!-- Taggar: -->';
+											foreach ( $cats as $category) { 
+												echo '<a href="'.get_category_link( $category->term_id ).'"><span class="label label-info">'.$category->cat_name.'</span></a> ';
+											} 
+											echo '</p>';
+										}
+										?>
+                                                                                </p>
+                                                                        </div>
+                                                                </div>
+
+							</div>
+						</div>
+					</div>
+				</section>
+                                <?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+<!--
+<? if( function_exists('wp_pagenavi') ) wp_pagenavi();
+?>
+-->
+
+<?
 } else {
 ?>
 
@@ -103,7 +171,7 @@ if (!is_front_page()) {
 						<div class="news-box index-updates"> <!-- NEWS BOX -->
 							<!-- <?php $id = get_the_ID(); echo get_the_post_thumbnail($id, array(400,200)); ?>
 							-->
-							<h3><? echo the_title(); ?></h3>	
+							<h3>a<a href="/"><? echo the_title(); ?></a></h3>	
 							<h6><?php the_time( get_option( 'date_format' ) ); ?></h6>
 							<article>
 								<p><?php the_excerpt();?></p>
