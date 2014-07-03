538b0205e2ccad0e1307df8991f3347c679e179c
Marcus Danielsson <marcus@karmint.com>
2014-07-03 22:45:39 +0200

    ny mekaniker

--- wp-content/themes/yoiride/page-team.php
+++ wp-content/themes/yoiride/page-team.php
@@ -126,23 +126,26 @@ get_header();
 				<section class="team-profiles">
 					<div class="row">
 						<div class="col-md-5">
-							<img src="<?php bloginfo('template_url'); ?>/img/mekaniker_profil.jpg">
-							<h4 class="text-center">Kanske Dusson?</h4>
+							<img src="<?php bloginfo('template_url'); ?>/img/peter_profil.png">
+							<h4 class="text-center">Peter Böröczki</h4>
 							<hr>
 							<h5 class="profile-info text-center">
-							Ålder: 18+<br><br>
-							Bor: Gärna i Stockholmsområdet då det förenklar resor<br><br>
-							Erfarenhet: Ingen tidigare erfarenhet krävs
+							Ålder: 50<br><br>
+							Bor: Västerås<br><br> 
+							Familj: Fru och 2 barn, tjejer som båda kör morcykel :) </br></br>
+
 							</h5>
 							<hr class="visible-xs">
 						</div>
 						<div class="col-md-7">
-							<p>Som mekaniker hos YOIRIDE blir man en väldigt viktig del i vår racing, det är vi tillsammans som kommer att skörda vinster, land och rike runt!</p>
-							<p>Den mekaniker vi söker tycker att det är riktigt kul med motorsport och gillar att resa, kränga däck, tanka våra hojar, hålla koll på våra loggningssystem den ska även tycka om att hjälpa våra kunder. Alltid med ett leende på läpparna!
+							<p>Intresse för motorcyklar började i 8 års åldern när min pappa byggde min första motorcykel som var 175 ccm.
+							Började bygga, trimma motorcyklar själv i 14 års ålder samtidigt började köra cross. </p>
+							<p>Skaffade körkort när jag var 17 år, då började jag köra enduro. Körde enduro för Ungerska landslaget, kört Sixdays /enduro VM/ 1987. </p>
+							<p>Även provat mc stunt, som jag kört i 8 år på uppvisningar och tävlingar.
 							</p>
-							<p>Flyter allt på under sommarsäsongen kommer det även finnas möjlighet att resa med oss till Spanien i höst och vinter.
+							<p>2007 provade jag roadracing för första gången och fastnade direkt. Det roligaste mc sport jag har kört. Kört några bandagar och 2013 körde jag hela serien i rookie 1000. Kom 2:a placering i serien.
 							</p>
-							<p>Slå en signal till Nils om du eller någon du känner skulle kunna passa in i gänget! <a href="tel:+46709346901">0709-346 901</a>
+							<p>Det andra stora intresset jag har är att skruva motorcyklar. Eftersom att jag har valt att inte tävla i år, kommer jag med nöje hjälpa till som mekaniker för team YOIRIDE.
 							</p>
 						</div>
 					</div>
